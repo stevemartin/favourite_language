6c614d0785861e85ef657b3512ce7753a321ae1d
Martian Sleeves <martian.sleeves@gmail.com>
2014-08-22 09:43:45 +0100

    naive implementation

--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -3,10 +3,19 @@ require './script'
 
 describe GetFavoriteLanguage do
 
-  subject { described_class.new "stevemartin" }
+  let(:github_user) { "stevemartin" }
+  subject { described_class.new(github_user) }
 
-  it "should return my favorite language" do
+  it "returns my favorite language" do
     expect( subject.result ).to eq("Ruby")
+  end
+
+  context "with a different username" do
+    let(:github_user) { "torvalds" }
+
+    it "returns Linus' favorite language" do
+      expect( subject.result ).to eq("C")
+    end
   end
 
 end
