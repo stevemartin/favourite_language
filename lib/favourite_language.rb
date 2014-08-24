2f29801a8f5ce1d1374cff2ebcc3ba6a9518d258
Martian Sleeves <martian.sleeves@gmail.com>
2014-08-24 12:26:45 +0100

    refactor favorite language class * add edge cases

--- lib/favourite_language.rb
+++ lib/favourite_language.rb
@@ -1,9 +1,13 @@
 # Get a github users favourite programming langauage
 # Public class with as simple two call interface
 #
-# Pass in a valid github username and an api client to new
+# Initialize with a valid github username and an api client.
 #
 # Call results to get the answer.
+#
+# This class cares about computing language data for
+# a github user, retreived using an api client adapter, and retreiving the users favorite language.
+#
 class FavouriteLanguage
   attr_reader :github_user
   def initialize(github_user, client)
@@ -11,13 +15,46 @@ class FavouriteLanguage
     @client      = client
   end
 
-  def results
-    favourite_language
+  def get
+    response = get_github_user_data
+
+    return '404 - User not found!' if response == '404'
+    with_max_loc(response)
   end
 
   private
 
-  def favourite_language
-    @client.favourite_language_for(github_user)
+  def with_max_loc(response)
+    repos     = users_repos(response)
+    counts    = language_counts(repos)
+    max       = counts.invert.keys.sort.reverse.first
+    results   = counts.reject {|_key, value| value < max }
+    languages = results.keys
+
+    return languages.first if languages.size < 2
+    languages
+  end
+
+  def users_repos(response)
+    @client.get_users_repos(response) # Gives us an array of the users repos from the client.
+  end
+
+  def language_counts(repos)
+    languages     = Hash.new
+    api_languages = @client.get_repo_languages(repos)
+
+    api_languages.map do |hash|
+      languages.merge!(hash) { |_key, v1, v2| v1 + v2 }
+    end
+    languages
+  end
+
+  def get_github_user_data
+    begin
+      response = @client.get_github_user(github_user)
+    rescue
+      return '404'
+    end
+    response
   end
 end
--- spec/favourite_language_spec.rb
+++ spec/favourite_language_spec.rb
@@ -3,15 +3,51 @@ require_relative '../lib/favourite_language'
 
 describe FavouriteLanguage do
 
-  let(:favourite_language) { 'APL' }
-  let(:api_client)  { double(:api_client) }
-  let(:github_user) { 'cybercymon' }
+  let(:results) { :APL }
+  let(:api_client)         { double(:api_client) }
+  let(:github_user)        { 'cybercymon' }
+  let(:repos)              { [double]*3 }
+  let(:languages)          { [{:APL => 5, :COBAL => 4, :Python =>3}]*3 }
+
   subject { described_class.new(github_user, api_client) }
 
-  it 'calls the api client and returns the favourite' do
-    allow(api_client).to receive(:favourite_language_for)
-      .with(github_user).and_return(favourite_language)
+  before do
+    allow(api_client).to receive(:get_github_user)
+      .with(github_user).and_return(github_user)
+    allow(api_client).to receive(:get_users_repos)
+      .with(github_user).and_return(repos)
+    allow(api_client).to receive(:get_repo_languages)
+      .with(repos).and_return(languages)
+  end
 
-    expect(subject.results).to eq(favourite_language)
+  it 'calls the api client and returns the favourite' do
+    expect(subject.get).to eq(results)
+  end
+
+  context 'when there are two equal results' do
+    let(:languages) { [{:APL => 5, :COBAL => 5, :Python =>3}]*3 }
+    let(:results) { [:APL, :COBAL] }
+
+    it 'returns the two' do
+      expect(subject.get).to eq(results)
+    end
+  end
+
+  context 'when the results are empty' do
+    let(:languages) { [] }
+    let(:results) { nil }
+
+    it 'returns nil' do
+      expect(subject.get).to eq(results)
+    end
+  end
+
+  context 'when there are none for each project' do
+    let(:languages) { [{}]*3 }
+    let(:results) { nil }
+
+    it 'returns nil' do
+      expect(subject.get).to eq(results)
+    end
   end
 end
